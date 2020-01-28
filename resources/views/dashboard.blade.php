0766cf5892eb8193f2bd012a8b1353e2d0d4c615
Edgar Enrique Tek Torres <edgar_tek_torres@hotmail.com>
2020-01-28 13:04:16 -0600

    Change dashboard view, add last users

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,15 @@
-dashboard.blade.php
+@extends('layouts.app')
+
+@section('title', 'Resume')
+
+
+@section('content')
+	<ul class="list-group">
+		@foreach($users as $user)
+			<li class="list-group-item">
+				<h4>{{$user->name}}</h4>
+				<em>Último Ingreso: ---</em>
+			</li>
+		@endforeach
+	</ul>
+@endsection
