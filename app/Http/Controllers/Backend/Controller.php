24d82ce6f8729710df77568084a51a8fef12d736
Edgar Enrique Tek Torres <edgar_tek_torres@hotmail.com>
2020-01-28 14:00:47 -0600

    Configuration of Controller.php in Backend folder

--- app/Http/Controllers/Backend/Controller.php
+++ app/Http/Controllers/Backend/Controller.php
@@ -1,1 +1,14 @@
-Controller.php
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller as Base;
+
+class Controller extends Base
+{
+    	
+	public function __construct()
+	{
+		$this->middleware('auth');	
+	}
+}
