9336f855f17ebca7c9b988d2a16e312e3b6393a6
Edgar Enrique Tek Torres <edgar_tek_torres@hotmail.com>
2020-01-28 12:44:26 -0600

    Added User model and $user variable

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\User;
 
 class DashboardController extends Controller
 {
-    //
+    public function index(User $user ){
+
+    	return view('dashboard', [
+    		'users' => $user->take(5)->get()
+    	]);
+    }
 }
